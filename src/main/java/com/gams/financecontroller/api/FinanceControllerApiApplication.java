a9d2d70f2f91983e7024e807c00478e37434fee8
gams <gabriel.antonioms@gmail.com>
2021-04-23 00:41:29 -0300

    first commit

---
+++ src/main/java/com/gams/financecontroller/api/FinanceControllerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gams.financecontroller.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControllerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceControllerApiApplication.class, args);
+    }
+
+}
