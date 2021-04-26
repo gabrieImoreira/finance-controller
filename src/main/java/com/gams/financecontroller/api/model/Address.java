31bd5b6b0cc542d18a7827cbbd1b0f7208e7f4c3
gams <gabriel.antonioms@gmail.com>
2021-04-25 23:55:13 -0300

    add Person and Address classes

---
+++ src/main/java/com/gams/financecontroller/api/model/Address.java
@@ -0,0 +1,72 @@
+package com.gams.financecontroller.api.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String publicArea; //logradouro
+    private String number;
+    private String complement;
+    private String district; //bairro
+    private String zipCode; //cep
+    private String city;
+    private String state;
+
+
+    public String getPublicArea() {
+        return publicArea;
+    }
+
+    public void setPublicArea(String publicArea) {
+        this.publicArea = publicArea;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/gams/financecontroller/api/model/Person.java
@@ -0,0 +1,65 @@
+package com.gams.financecontroller.api.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @NotNull
+    @Size(min = 2, max = 150)
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @NotNull
+    private Boolean active;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(name, person.name) && Objects.equals(address, person.address) && Objects.equals(active, person.active);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, active);
+    }
+}
