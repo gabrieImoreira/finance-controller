eea6cf3d6fcc62047da63e8d482069c81eef80b3
gams <gabriel.antonioms@gmail.com>
2021-05-03 01:59:39 -0300

    add POST in Input

--- src/main/java/com/gams/financecontroller/api/resource/InputResource.java
+++ src/main/java/com/gams/financecontroller/api/resource/InputResource.java
@@ -1,15 +1,17 @@
 package com.gams.financecontroller.api.resource;
 
+import com.gams.financecontroller.api.event.ResourceCreatedEvent;
 import com.gams.financecontroller.api.model.Input;
 import com.gams.financecontroller.api.model.Person;
 import com.gams.financecontroller.api.repository.InputRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +21,9 @@ public class InputResource {
 
     @Autowired
     InputRepository inputRepository;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping
     public List<Input> listAll(){
@@ -30,4 +35,12 @@ public class InputResource {
         Optional<Input> savedId  = inputRepository.findById(id);
         return !savedId.isEmpty() ? ResponseEntity.ok(savedId) : ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+    public ResponseEntity<Input> create(@Valid @RequestBody Input input, HttpServletResponse response){
+        Input savedInput = inputRepository.save(input);
+        publisher.publishEvent(new ResourceCreatedEvent(this, response, savedInput.getId())); 
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedInput);
+    }
+
 }
